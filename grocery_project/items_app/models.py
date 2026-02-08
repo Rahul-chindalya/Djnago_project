b52a7497853e85ebcc21c46bc7abcc60b3680d55
Rahul-chindalya <rahulchindalya01@gmail.com>
2026-02-08 11:29:02 +0530

    created Items model  and superuser

--- grocery_project/items_app/models.py
+++ grocery_project/items_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Items(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=8,decimal_places=2)
+    discount = models.IntegerField()
+    available_quantity = models.IntegerField()
+    image = models.ImageField(upload_to='product/',null=True,blank=True)
+
+    def __str__(self):
+        return self.name 
